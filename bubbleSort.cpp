c40f53940c9225d1a085633b4ab3a02b4bdf6622
SoHyeon Kim <happythgus89@gmail.com>
2021-10-16 16:45:28 +0900

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+void swap(int x, int y) {
+	int temp = x;
+	x = y;
+	y = temp;	
+}
+
+void bubble(int a[], int n) {
+	int k = 0;
+	while (k < n - 1) {
+		int j;
+		int last = n - 1;
+		for (j = n - 1;j > k;j--) {
+			if (a[j - 1] > a[j]) {
+				swap(a[j - 1], a[j]);
+				last = j;
+			}
+		}
+		k = last;
+	}
+}
+
+int main() {
+	int arr[]= { 1, 3, 7, 5, 9 };
+	bubble(arr, 5);
+	for (int i = 0;i < 5;i++) {
+		std::cout << arr[i] << std::endl;
+	}
+	return 0;
+}
+
+/*
+
+[Output]
+
+1
+3
+7
+5
+9
+
+//정렬이 안되는데,,?
+
+*/
